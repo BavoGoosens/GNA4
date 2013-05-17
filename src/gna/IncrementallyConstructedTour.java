06621e118c824d5789818ded82990daf5c035980
Bavo Goosens <bavo.goosens@gmail.com>
2013-05-17 16:07:18 +0200

    begin insert

--- src/gna/IncrementallyConstructedTour.java
+++ src/gna/IncrementallyConstructedTour.java
@@ -1,20 +1,33 @@
 package gna;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 /**
  * Tours that are constructed incrementally by inserting the elements in the
  * given world one by one.
  */
 public abstract class IncrementallyConstructedTour extends Tour {
 
-  public IncrementallyConstructedTour(World world) {
-    super(world);
-    /**
-     * Note: we expect the elements to be inserted in the same order they are added to the world.
-     */
-    for (Point point : world.getPoints()) {
-      insert(point);
-    }
-  }
+	protected ArrayList<Point> tour = new ArrayList<Point>();
 
-  protected abstract void insert(Point point);
+	public ArrayList<Point> getTour() {
+		return tour;
+	}
+
+	public void setTour(ArrayList<Point> tour) {
+		this.tour = tour;
+	}
+
+	public IncrementallyConstructedTour(World world) {
+		super(world);
+		/**
+		 * Note: we expect the elements to be inserted in the same order they are added to the world.
+		 */
+		for (Point point : world.getPoints()) {
+			insert(point);
+		}
+	}
+
+	protected abstract void insert(Point point);
 }
--- src/gna/NearestNeighborTour.java
+++ src/gna/NearestNeighborTour.java
@@ -6,23 +6,36 @@ import java.util.List;
  * A tour constructed using the nearest neighbor heuristic.
  */
 public class NearestNeighborTour extends IncrementallyConstructedTour {
-  
-  public NearestNeighborTour(World world) {
-    super(world);
-  }
-  
-  @Override
-  public void insert(Point point) {
-	  // shizzles
-  }
 
-  @Override
-  public double getTotalDistance() {
-    // TODO
-  }
+	public NearestNeighborTour(World world) {
+		super(world);
+	}
 
-  @Override
-  public List<Point> getVisitSequence() {
-    throw new RuntimeException("not implemented");
-  }
+	@Override
+	public void insert(Point point) {
+		// ieder punt toevoegen bij zijn dichtsbijzijnde buur
+		if(super.getTour().isEmpty()){
+			super.getTour().add(point);
+		}else{	  
+			int index = 0;
+			double minimum = point.distanceTo(super.getTour().get(0));
+			for(Point punt: super.getTour()){
+				if
+			}
+		}
+	}
+
+	@Override
+	public double getTotalDistance() {
+		if (getWorld().getNbPoints() < 2)
+			return 0;
+		else {
+
+		}
+	}
+
+	@Override
+	public List<Point> getVisitSequence() {
+
+	}
 }
